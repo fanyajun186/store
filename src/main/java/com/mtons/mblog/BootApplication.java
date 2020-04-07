4d9cf62df7d57c7710dbca335ea53050e4911610
yajun.fan <yajun.fan@mljr.com>
2020-04-07 17:23:50 +0800

    测试

--- src/main/java/com/mtons/mblog/BootApplication.java
+++ src/main/java/com/mtons/mblog/BootApplication.java
@@ -1,10 +1,15 @@
 package com.mtons.mblog;
 
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ApplicationContext;
+
+import com.mtons.mblog.study.service.Van;
 
 /**
  * SprintBootApplication
@@ -12,12 +17,20 @@ import org.springframework.context.ApplicationContext;
 @Slf4j
 @SpringBootApplication
 @EnableCaching
-public class BootApplication {
+public class BootApplication implements CommandLineRunner{
+	
+	@Autowired
+    private Van van;
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(BootApplication.class, args);
         String serverPort = context.getEnvironment().getProperty("server.port");
-        log.info("mblog started at http://localhost:" + serverPort);
+        log.info("youcaige shop started at http://localhost:" + serverPort);
     }
+
+	@Override
+	public void run(String... args) throws Exception {
+		van.fight();		
+	}
 
 }
